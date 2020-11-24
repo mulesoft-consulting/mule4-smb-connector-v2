498bccd7357d6fcaa1785ea92f60edb9a8199c05
olamiral-mulesoft <olamiral@mulesoft.com>
2020-11-24 12:26:24 -0300

    Certification Checklist
    Added licence header to source code files

--- src/main/java/org/mule/extension/smb/internal/utils/SmbUtils.java
+++ src/main/java/org/mule/extension/smb/internal/utils/SmbUtils.java
@@ -1,8 +1,5 @@
-/*
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
+/**
+ * (c) 2003-2020 MuleSoft, Inc. The software in this package is published under the terms of the Commercial Free Software license V.1 a copy of which has been included with this distribution in the LICENSE.md file.
  */
 package org.mule.extension.smb.internal.utils;
 
--- src/test/java/org/mule/extension/AbstractSmbTestHarness.java
+++ src/test/java/org/mule/extension/AbstractSmbTestHarness.java
@@ -1,3 +1,6 @@
+/**
+ * (c) 2003-2020 MuleSoft, Inc. The software in this package is published under the terms of the Commercial Free Software license V.1 a copy of which has been included with this distribution in the LICENSE.md file.
+ */
 package org.mule.extension;/*
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  * The software in this package is published under the terms of the CPAL v1.0
