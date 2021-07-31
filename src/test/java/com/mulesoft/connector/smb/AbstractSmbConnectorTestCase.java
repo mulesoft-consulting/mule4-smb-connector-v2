901884fef0f351297ad9f85174f258977c0ce83a
olamiral-mulesoft <olamiral@mulesoft.com>
2021-07-31 16:14:06 -0300

    Certification Checklist
    - Fixed pom parent
    - Fixed license files (LICENSE.md and LICENSE_HEADER.txt)
    - Removed formatter.xml file and settings in pom.xml

--- src/test/java/com/mulesoft/connector/smb/AbstractSmbConnectorTestCase.java
+++ src/test/java/com/mulesoft/connector/smb/AbstractSmbConnectorTestCase.java
@@ -1,8 +1,7 @@
-/*
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
+/**
+ * (c) 2003-2021 MuleSoft, Inc. The software in this package is
+ * published under the terms of the Commercial Free Software license V.1, a copy of which
+ * has been included with this distribution in the LICENSE.md file.
  */
 package com.mulesoft.connector.smb;
 
